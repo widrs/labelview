7909385311c4d0f419ae989f60c822c2941f16f7
widders <octet.tube@gmail.com>
2024-11-27 10:37:11 -0800

    open_create is the default

--- src/lib.rs
+++ src/lib.rs
@@ -17,10 +17,7 @@ pub fn connect() -> Result<Connection> {
     let data_dir = get_data_dir()?;
     std::fs::create_dir_all(&data_dir)
         .map_err(|err| anyhow!("couldn't create data directory {data_dir:?}: {err}"))?;
-    let mut db = Connection::open_with_flags(
-        data_dir.join("data.sqlite"),
-        rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE | rusqlite::OpenFlags::SQLITE_OPEN_CREATE,
-    )?;
+    let mut db = Connection::open(data_dir.join("data.sqlite"))?;
     db.set_db_config(
         rusqlite::config::DbConfig::SQLITE_DBCONFIG_ENABLE_FKEY,
         true,
