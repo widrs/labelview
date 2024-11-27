f44cbeca301b05a84998ea9b99a74b98e1c782f5
widders <octet.tube@gmail.com>
2024-11-27 14:39:32 -0800

    tweak tables: multiple label records exist per seq

--- migrations/V001__label_records.sql
+++ migrations/V001__label_records.sql
@@ -1,15 +1,16 @@
 CREATE TABLE IF NOT EXISTS label_records(
-   src TEXT NOT NULL,
-   seq INT NOT NULL,
-   create_timestamp TEXT NOT NULL,
-   expiry_timestamp TEXT,
-   neg BOOL NOT NULL,
-   target_uri TEXT NOT NULL,
-   target_cid TEXT,
-   val TEXT NOT NULL,
-   sig BLOB
+    id INTEGER PRIMARY KEY,
+    src TEXT NOT NULL,
+    seq INTEGER NOT NULL,
+    create_timestamp TEXT NOT NULL,
+    expiry_timestamp TEXT,
+    neg BOOL NOT NULL,
+    target_uri TEXT NOT NULL,
+    target_cid TEXT,
+    val TEXT NOT NULL,
+    sig BLOB
 );
-CREATE UNIQUE INDEX IF NOT EXISTS label_records_seq
-ON label_records (src, seq);
-CREATE UNIQUE INDEX IF NOT EXISTS label_records_target
-ON label_records (target_uri, target_cid, val, neg);
+CREATE UNIQUE INDEX IF NOT EXISTS label_records_outgoing
+ON label_records (src, val, seq, target_uri, target_cid);
+CREATE INDEX IF NOT EXISTS label_records_incoming
+ON label_records (target_uri, val);
