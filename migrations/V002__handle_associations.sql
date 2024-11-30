7b438a22042dcc4e09b404b15d161acb190abe98
widders <octet.tube@gmail.com>
2024-11-30 01:59:32 -0800

    fix handle associations table def

--- migrations/V002__handle_associations.sql
+++ migrations/V002__handle_associations.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS known_handles(
     did TEXT NOT NULL,
     handle TEXT NOT NULL,
-    witnessed_timestamp TEXT NOT NULL
+    witnessed_timestamp TEXT NOT NULL,
     PRIMARY KEY (did, handle)
 ) WITHOUT ROWID;
 CREATE UNIQUE INDEX IF NOT EXISTS handle_dids
