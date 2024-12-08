5e10948e7936d81ee8d0b4d64b4d112457941802
widders <octet.tube@gmail.com>
2024-12-07 23:30:49 -0800

    update migrations

---
+++ migrations/V001__init.sql
@@ -0,0 +1,48 @@
+CREATE TABLE IF NOT EXISTS imports(
+    id INTEGER PRIMARY KEY,
+    start_time TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS label_records(
+    id INTEGER PRIMARY KEY,
+    src TEXT NOT NULL,
+    seq INTEGER NOT NULL,
+    create_timestamp TEXT NOT NULL,
+    expiry_timestamp TEXT,
+    neg BOOL NOT NULL,
+    target_uri TEXT NOT NULL,
+    target_cid TEXT,
+    val TEXT NOT NULL,
+    import_id INTEGER NOT NULL,
+    last_seen_timestamp TEXT NOT NULL
+);
+CREATE UNIQUE INDEX IF NOT EXISTS label_records_outgoing
+    ON label_records (src, val, target_uri, seq);
+CREATE INDEX IF NOT EXISTS label_records_incoming
+    ON label_records (target_uri, val);
+
+CREATE TABLE IF NOT EXISTS suspicious_records(
+    id INTEGER PRIMARY KEY,
+    import_id INTEGER NOT NULL,
+    seen_timestamp TEXT NOT NULL,
+    problem_category TEXT NOT NULL,
+    src TEXT NOT NULL,
+    seq INTEGER NOT NULL,
+    create_timestamp TEXT NOT NULL,
+    expiry_timestamp TEXT,
+    neg BOOL NOT NULL,
+    target_uri TEXT NOT NULL,
+    target_cid TEXT,
+    val TEXT NOT NULL
+);
+CREATE INDEX IF NOT EXISTS suspicious_records_by_import
+    ON suspicious_records(import_id, problem_category);
+
+CREATE TABLE IF NOT EXISTS known_handles(
+    did TEXT NOT NULL,
+    handle TEXT NOT NULL,
+    witnessed_timestamp TEXT NOT NULL,
+    PRIMARY KEY (did, handle)
+) WITHOUT ROWID;
+CREATE UNIQUE INDEX IF NOT EXISTS handle_dids
+    ON known_handles(handle, did);
--- migrations/V002__handle_associations.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS known_handles(
-    did TEXT NOT NULL,
-    handle TEXT NOT NULL,
-    witnessed_timestamp TEXT NOT NULL,
-    PRIMARY KEY (did, handle)
-) WITHOUT ROWID;
-CREATE UNIQUE INDEX IF NOT EXISTS handle_dids
-    ON known_handles(handle, did);
